d845ad73bf1d3115967c90c45e6078f76722370b
sitesite90 <104436046+sitesite90@users.noreply.github.com>
2022-04-26 20:46:12 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -188,10 +188,9 @@ function App() {
         flex={1}
         ai={"center"}
         style={{ padding: 24, backgroundColor: "#bda392" }}
-        image={CONFIG.SHOW_BACKGROUND ? "/config/images/bg.png" : null}
       >
         <a href={CONFIG.MARKETPLACE_LINK}>
-          <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
+          <StyledLogo alt={"logo"} src={"/config/images/logo-camroll.png"} />
         </a>
         <s.SpacerSmall />
         <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
