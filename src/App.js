ec8a19b4a7ffe006c4753b094f2296affcfedd02
sitesite90 <104436046+sitesite90@users.noreply.github.com>
2022-04-27 10:47:37 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -360,7 +360,7 @@ function App() {
                         disabled={claimingNft ? 1 : 0}
                         onClick={(e) => {
                           e.preventDefault();
-                          claimNFTs();
+                          mint();
                           getData();
                         }}
                       >
